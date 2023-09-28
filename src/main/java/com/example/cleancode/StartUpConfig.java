1cb834dc81005b921bef117f66b7f1e0726ff19c
Segtm <Segtmednymail@gmail.com>
2023-09-28 14:40:05 +0200

    test, hoppas inget fuckr upp

--- src/main/java/com/example/cleancode/StartUpConfig.java
+++ src/main/java/com/example/cleancode/StartUpConfig.java
@@ -1,5 +1,7 @@
 package com.example.cleancode;
 
+import com.example.cleancode.availability.Availability;
+import com.example.cleancode.availability.AvailabilityRepository;
 import com.example.cleancode.customer.Customer;
 import com.example.cleancode.customer.CustomerRepository;
 import com.example.cleancode.employees.Employee;
@@ -20,10 +22,14 @@ public class StartUpConfig {
 
     private final EmployeeRepository employeeRepository;
     private final CustomerRepository customerRepository;
+    private final AvailabilityRepository availabilityRepository;
+    private final JobRepository jobRepository;
 
-    public StartUpConfig(EmployeeRepository employeeRepository, CustomerRepository customerRepository) {
+    public StartUpConfig(EmployeeRepository employeeRepository, CustomerRepository customerRepository, AvailabilityRepository availabilityRepository, JobRepository jobRepository) {
         this.employeeRepository = employeeRepository;
         this.customerRepository = customerRepository;
+        this.availabilityRepository = availabilityRepository;
+        this.jobRepository = jobRepository;
     }
 
     @Bean
@@ -81,6 +87,24 @@ public class StartUpConfig {
             jobRepository.save(new Job(Jobtype.BASIC, "2023-11-24T12:00", JobStatus.PENDING,
                     55, PaymentOption.KLARNA, employeeRepository.findById(1L).get(),
                     customerRepository.findById(1L).get()));
+        };
+    }
+
+    @Bean
+    public CommandLineRunner initAvailabilityDB(AvailabilityRepository availabilityRepository){
+        return args -> {
+            Availability availability = new Availability();
+            availability.setStartTime(LocalDateTime.of(2023, Month.NOVEMBER, 24, 9, 0));
+            availability.setEndTime(LocalDateTime.of(2023, Month.NOVEMBER, 24, 17, 0));
+
+            Employee employee1 = employeeRepository.findById(1L).get();
+            Employee employee2 = employeeRepository.findById(2L).get();
+            availability.setEmployees(List.of(employee1, employee2));
+
+            Job job1 = jobRepository.findById(1L).get();
+            availability.setJobs(List.of(job1));
+
+            availabilityRepository.save(availability);
         };
     }
 }
