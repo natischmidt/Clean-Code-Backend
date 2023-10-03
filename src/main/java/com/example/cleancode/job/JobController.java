407de8348b88f5205b0ad4280f2ff04d47a4a721
mnell <mtothenell@gmail.com>
2023-10-03 16:47:59 +0200

    - Lagt till crossorigion i Job-entity /Ola.

--- src/main/java/com/example/cleancode/job/JobController.java
+++ src/main/java/com/example/cleancode/job/JobController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/jobs")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class JobController {
 
     private final JobService jobService;
