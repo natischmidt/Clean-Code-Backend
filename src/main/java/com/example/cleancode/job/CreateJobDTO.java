a2bca46a29b5d9e0f5990bdb2718d3f56b2bb791
Jimmy Larsson <jimmy.larsson@europe.com>
2023-10-04 12:47:50 +0200

    Ändrade timeslot to List<Timeslot> i CreateJobDTO. Uppdaterade JobService så att man kan boka flera timeslot på en gång job entity innehåller fortfarande endast starttid. Vi behöver fixa så att man kan kolla flera timeslots när kunden ska ha upp tillgängliga tider på en viss dag.

--- src/main/java/com/example/cleancode/job/CreateJobDTO.java
+++ src/main/java/com/example/cleancode/job/CreateJobDTO.java
@@ -1,13 +1,9 @@
 package com.example.cleancode.job;
 
-import com.example.cleancode.enums.JobStatus;
 import com.example.cleancode.enums.Jobtype;
 import com.example.cleancode.enums.PaymentOption;
 import com.example.cleancode.enums.TimeSlots;
 import lombok.Data;
-
-import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Data
