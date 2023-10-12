700355ccb5170c92a765e2ce97f195be5ea64dc4
Jimmy Larsson <jimmy.larsson@europe.com>
2023-10-12 15:57:59 +0200

    Fixade lite med updateJob. Nu ska det fungera att ta bort ett jobb på rätt sätt. uppdatera status på ett jobb leder till en metod där mail skickas bland annat. Mer ska kanske in där.

--- src/main/java/com/example/cleancode/job/GetAvailableEmployeeDTO.java
+++ src/main/java/com/example/cleancode/job/GetAvailableEmployeeDTO.java
@@ -1,9 +1,6 @@
 package com.example.cleancode.job;
 
-import com.example.cleancode.enums.TimeSlots;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 @Data
 public class GetAvailableEmployeeDTO {
