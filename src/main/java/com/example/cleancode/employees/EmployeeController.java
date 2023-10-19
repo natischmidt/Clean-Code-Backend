87879314b6ee10febd000ceee2fd3e201415c0e9
Segtm <Segtmednymail@gmail.com>
2023-10-19 13:42:01 +0200

    fix

--- src/main/java/com/example/cleancode/employees/EmployeeController.java
+++ src/main/java/com/example/cleancode/employees/EmployeeController.java
@@ -24,7 +24,7 @@ public class EmployeeController {
         return employeeService.deleteEmployee(empId);
     }
 
-    @PostMapping("/getEmployee")
+    @GetMapping("/getEmployee")
     public GetEmployeeDTO getEmployee(@RequestHeader Long empId) {
         return employeeService.getEmployee(empId);
     }
