b22f45f8902f28ad290b75a44d7c37452bd69eba
nati schmidt <nati.schmidt97@outlook.de>
2023-11-14 11:02:57 +0100

    Clean code up in folder *job*

--- src/main/java/com/example/cleancode/exceptions/JobDoesNotExistException.java
+++ src/main/java/com/example/cleancode/exceptions/JobDoesNotExistException.java
@@ -1,7 +1,6 @@
 package com.example.cleancode.exceptions;
 
 public class JobDoesNotExistException extends RuntimeException{
-
     public JobDoesNotExistException(String message) {
         super(message);
     }
